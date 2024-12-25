723ab5c5324db8e9fef4ed0c427819716cd8f925
yuki.osanai <ykosni.ykosni@gmail.com>
2024-12-25 02:25:54 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,6 +47,12 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        //バリデーション
+        $request->validate([
+           
+            'content' => 'required|max:255',
+            ]);
+        
         //タスクを作成
         $task = new Task;
         $task->content = $request->content;
@@ -93,6 +99,11 @@ class TasksController extends Controller
      */
     public function update(Request $request, string $id)
     {
+        //バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+            ]);
+        
         //idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         //タスクを更新
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="ja">
     <head>
         <meta charset="utf-8">
-        <title>MessageBoard</title>
+        <title>Tasklist</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link href="https://cdn.jsdelivr.net/npm/daisyui@4.6.1/dist/full.min.css" rel="stylesheet" type="text/css" />
